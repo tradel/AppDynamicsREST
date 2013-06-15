52e436ad09e0e14de78701e31f526b90baacb487
Todd Radel <tradel@appdynamics.com>
2013-06-14 23:26:05 -0400

    Changed to use Distribute, added requirements and data files

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@
 
 # __author__ = 'Todd Radel <tradel@appdynamics.com>'
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='AppDynamicsREST',
-      version='0.1.0',
+      version='0.1.2',
       description='REST API client for AppDynamics Controllers',
       author='Todd Radel',
       author_email='tradel@appdynamics.com',
-      url='http://www.appdynamics.com',
+      url='https://github.com/tradel/AppDynamicsREST',
+      download_url='https://github.com/tradel/AppDynamicsREST',
       packages=['appd'],
-      requires=['requests']
+      package_data={'appd': ['README.md', 'data/*', 'examples/*']},
+      install_requires=['requests', 'lxml', 'tzlocal']
       )
