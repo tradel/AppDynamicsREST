7200edb1d4b7eafcb51bc6c93d6000bdc9d57ca4
Todd Radel <tradel@appdynamics.com>
2015-02-03 08:50:09 -0500

    Added docstrings for Sphinx

--- appd/__init__.py
+++ appd/__init__.py
@@ -1,1 +1,13 @@
+"""
+Python SDK for AppDynamics REST API
+
+.. moduleauthor:: Todd Radel <tradel@appdynamics.com>
+"""
+
+import model
+import request
+import cmdline
+import time
+
+__VERSION__ = '0.3.0-dev'
 __author__ = 'Todd Radel <tradel@appdynamics.com>'
