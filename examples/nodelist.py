cf18a9c134446e421d9cb8afdba80cf24ea9bd84
Todd Radel <tradel@appdynamics.com>
2015-02-03 09:29:14 -0500

    Sample script to print a simple list of all the nodes registered with the controller

---
+++ examples/nodelist.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+"""
+Sample script to print a simple list of all the nodes registered with the controller.
+Output format: app_name,tier_name,node_name,host_name
+
+.. moduleauthor:: Todd Radel <tradel@appdynamics.com>
+"""
+
+__author__ = 'Todd Radel <tradel@appdynamics.com>'
+
+from appd.cmdline import parse_argv
+from appd.request import AppDynamicsClient
+
+args = parse_argv()
+c = AppDynamicsClient(args.url, args.username, args.password, args.account, args.verbose)
+
+for app in c.get_applications():
+    for node in c.get_nodes(app.id):
+        print ','.join([app.name, node.tier_name, node.name, node.machine_name])
+
