64f7a76a737facfdfd526d46d4d3d851f23224f2
Todd Radel <tradel@appdynamics.com>
2015-04-23 17:11:59 -0400

    Updated for Python3 compatibility

--- appd/__init__.py
+++ appd/__init__.py
@@ -4,10 +4,11 @@ Python SDK for AppDynamics REST API
 .. moduleauthor:: Todd Radel <tradel@appdynamics.com>
 """
 
-import model
-import request
-import cmdline
-import time
-
 __VERSION__ = '0.4.0'
 __author__ = 'Todd Radel <tradel@appdynamics.com>'
+
+from . import model
+from . import request
+from . import cmdline
+from . import time
+
