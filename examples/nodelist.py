f54b7a7a6e14e6e565488114b0950f34c4b1acee
Todd Radel <tradel@appdynamics.com>
2015-04-23 17:50:23 -0400

    Updated for Python3 compatibility

--- examples/nodelist.py
+++ examples/nodelist.py
@@ -7,15 +7,16 @@ Output format: app_name,tier_name,node_name,host_name
 .. moduleauthor:: Todd Radel <tradel@appdynamics.com>
 """
 
-__author__ = 'Todd Radel <tradel@appdynamics.com>'
-
+from __future__ import print_function
 from appd.cmdline import parse_argv
 from appd.request import AppDynamicsClient
+
+__author__ = 'Todd Radel <tradel@appdynamics.com>'
 
 args = parse_argv()
 c = AppDynamicsClient(args.url, args.username, args.password, args.account, args.verbose)
 
 for app in c.get_applications():
     for node in c.get_nodes(app.id):
-        print ','.join([app.name, node.tier_name, node.name, node.machine_name])
+        print(','.join([app.name, node.tier_name, node.name, node.machine_name]))
 
