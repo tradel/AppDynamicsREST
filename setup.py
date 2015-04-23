456147de6fc0ebb48d7a9d89fb60b19bf39f5cd5
Todd Radel <tradel@appdynamics.com>
2015-04-23 17:59:37 -0400

    Converted README to reStructuredText format

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(*filenames, **kwargs):
             buf.append(f.read())
     return sep.join(buf)
 
-long_description = read('README.txt', 'CHANGES.txt')
+long_description = read('README.rst')
 
 setup(name='AppDynamicsREST',
       version=appd.__version__,
