b0379630969a452d39168d0ab4351c0d6d32c843
Todd Radel <tradel@appdynamics.com>
2016-02-26 14:42:16 -0500

    Reflected new app layout on demo2

--- test/test_v1_applications.py
+++ test/test_v1_applications.py
@@ -7,17 +7,18 @@ Unit tests for AppDynamics REST API
 
 from test import ApplicationApiTest
 
+
 class V1_ApplicationTest(ApplicationApiTest):
 
     def test_app_list(self):
         apps = self.c.get_applications()
-        self.assertEqual(len(apps), 1)
-        self.assertEqual(apps[0].id, 10)
-        self.assertEqual(apps[0].name, 'ACME Book Store Application')
+        self.assertEqual(len(apps), 4)
+        self.assertEqual(apps[0].id, 12)
+        self.assertEqual(apps[0].name, 'Movie Tickets Online')
 
     def test_get_app_by_name(self):
         apps = self.c.get_applications()
-        self.assertEqual(apps.by_name('ACME Book Store Application').id, 10)
+        self.assertEqual(apps.by_name('Movie Tickets Online').id, 12)
 
     def test_get_config_by_name(self):
         config = self.c.get_config()
