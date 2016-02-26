85cd43ce13ea285f7fb443acc61212bd2cd6e49c
Todd Radel <tradel@appdynamics.com>
2016-02-26 14:45:29 -0500

    Rewrote assertIn() and assertNotIn() for better compatibility with 2.7

--- test/test.py
+++ test/test.py
@@ -6,6 +6,8 @@ Unit tests for AppDynamics REST API
 """
 
 import unittest
+from unittest.util import safe_repr
+
 import appd
 import creds.localhost as creds
 
@@ -15,32 +17,32 @@ __version__ = '0.4.5'
 
 
 class ApplicationApiTest(unittest.TestCase):
-
     def setUp(self):
         self.c = appd.request.AppDynamicsClient(creds.url, creds.user, creds.password, creds.account)
 
-    def assertIn(self, test_value, expected_set):
-        """
-        Included for compatibility with Python 2.6
-        """
-        msg = "%s did not occur in %s" % (test_value, expected_set)
-        self.assert_(test_value in expected_set, msg)
+    def assertIn(self, member, container, msg=None):
+        """Just like self.assertTrue(a in b), but with a nicer default message."""
+        if member not in container:
+            standard_msg = '%s not found in %s' % (safe_repr(member),
+                                                   safe_repr(container))
+            self.fail(self._formatMessage(msg, standard_msg))
 
-    def assertNotIn(self, test_value, expected_set):
-        """
-        Included for compatibility with Python 2.6
-        """
-        msg = "%s was found in %s" % (test_value, expected_set)
-        self.assert_(test_value not in expected_set, msg)
+    def assertNotIn(self, member, container, msg=None):
+        """Just like self.assertTrue(a not in b), but with a nicer default message."""
+        if member in container:
+            standard_msg = '%s unexpectedly found in %s' % (safe_repr(member),
+                                                            safe_repr(container))
+            self.fail(self._formatMessage(msg, standard_msg))
 
 
 if __name__ == '__main__':
     from test_v1_applications import *
     from test_v2_accounts import *
     from test_v2_license_modules import *
-    suite = unittest.TestLoader()\
-        .loadTestsFromTestCase(V1_ApplicationTest)\
-        .loadTestsFromTestCase(V2_AccountTest)\
+
+    suite = unittest.TestLoader() \
+        .loadTestsFromTestCase(V1_ApplicationTest) \
+        .loadTestsFromTestCase(V2_AccountTest) \
         .loadTestsFromTestCase(V2_LicenseModuleTest)
 
     unittest.TextTestRunner(verbosity=2).run(suite)
